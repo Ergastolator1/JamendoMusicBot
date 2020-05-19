2eca14a10f56080e0c772bbe14c5e68809feaa3a
Ergastolator1 <53176531+Ergastolator1@users.noreply.github.com>
2020-05-19 09:48:11 +0200

    Update jmusic.py

--- jmusic.py
+++ jmusic.py
@@ -61,9 +61,9 @@ class YTDLSource(discord.PCMVolumeTransformer):
         return '**{0.title}**'.format(self)
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
+    async def from_url(cls, url, *, loop=None, stream=True):
         loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=stream))
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
         if 'entries' in data:
             # take first item from a playlist
