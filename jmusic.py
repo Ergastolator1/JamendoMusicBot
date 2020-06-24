2706c7fd225c8f94a6db7c256c5d34a8cc1f6ea2
Ergastolator1 <53176531+Ergastolator1@users.noreply.github.com>
2020-06-24 10:34:40 +0200

    Updating help text (again)

--- jmusic.py
+++ jmusic.py
@@ -95,7 +95,7 @@ class JamendoMusic(commands.Cog):
     @commands.command()
     async def lounge(self, ctx):
         """
-        Plays some Jamendo lounge music from a 24/7 radio station
+        Plays some Jamendo lounge music from a 24/7 radio station.
         """
 
         async with ctx.typing():
@@ -140,7 +140,7 @@ bot.remove_command("help")
 @bot.command(name="help")
 async def help(ctx):
     """Shows this message."""
-    embed = discord.Embed(title="Help", description=f"{ctx.prefix}about - About Jamendo Music\n{ctx.prefix}join <channel> - Joins a voice channel.\n{ctx.prefix}play <URL of the Jamendo song> - Play a song from Jamendo Music (only Jamendo URLs supported).\n{ctx.prefix}lounge - Plays some Jamendo lounge music from a 24/7 radio station - powered by Radio Attivo.\n{ctx.prefix}volume - Changes the player's volume.\n{ctx.prefix}leave - Stops and disconnects the bot from voice.\n{ctx.prefix}help - Shows this message.", color=0xff1e58)
+    embed = discord.Embed(title="Help", description=f"{ctx.prefix}about - About Jamendo Music\n{ctx.prefix}join <channel> - Joins a voice channel.\n{ctx.prefix}play <URL of the Jamendo song> - Play a song from Jamendo Music (only Jamendo URLs supported).\n{ctx.prefix}lounge - Plays some Jamendo lounge music from a 24/7 radio station.\n{ctx.prefix}volume - Changes the player's volume.\n{ctx.prefix}leave - Stops and disconnects the bot from voice.\n{ctx.prefix}help - Shows this message.", color=0xff1e58)
     embed.set_thumbnail(url="https://i.imgur.com/G2l6t3X.png")
     embed.set_author(name="Jamendo Music", url="https://www.jamendo.com/en/", icon_url="https://i.imgur.com/G2l6t3X.png")
     await ctx.send(embed=embed)
